3ca0e7c79c538bd1ae2784a33bed39a0db3822d8
nshutilancelot3 <nshutilancelot111@gmail.com>
2026-02-06 23:13:51 +0200

    javascript webscraping

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, movie) {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
+
